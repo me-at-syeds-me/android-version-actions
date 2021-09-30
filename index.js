517c876c4f087c0912ff3bb6cf0318c9ce1aa88f
me-at-syeds-me <me@syeds.me>
2021-09-30 22:08:00 +0300

    Fix console log

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ try {
         newGradle = data;
         if (versionCode.length > 0) {
             newGradle = newGradle.replace(versionCodeRegexPattern, `$1${versionCode}`);
-            console.log(`Set new versionCode : ${newVersionCode}`);
+            console.log(`Set new versionCode : ${versionCode}`);
         } else {
             const lastVersionCodeStr = newGradle.match(versionCodeRegexPattern)[2];
             console.log(`Current versionCode : ${lastVersionCodeStr}`);
